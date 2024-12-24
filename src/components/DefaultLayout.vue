14758c81aaaee116de3300f919cc13111eb84434
ssword <ssword@gmail.com>
2024-12-25 01:30:05 +0800

    modularized layout

---
+++ src/components/DefaultLayout.vue
@@ -0,0 +1,15 @@
+<template>
+
+   <div class="bg-gray-100 h-full">
+    <navbar />
+    <main>
+        <router-view></router-view>
+    </main>
+   </div>
+
+</template>
+
+<script setup>
+import Navbar from './Navbar.vue';
+
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -3,29 +3,37 @@ import Home from '../views/Home.vue';
 import MealsByName from '../views/MealsByName.vue';
 import MealsByLetter from '../views/MealsByLetter.vue';
 import MealsByIngredients from '../views/MealsByIngredients.vue';
+import DefaultLayout from '../components/DefaultLayout.vue';
 
 
 const routes = [
     {
         path: '/',
-        name: 'home',
-        component: Home,
-    },
-    {
-        path: '/by-name/:name?',
-        name: 'byName',
-        component: MealsByName,
-    },
-    {
-        path: '/by-letter/:letter?',
-        name: 'byLetter',
-        component: MealsByLetter,
-    },
-    {
-        path: '/by-ingredients/:ingredient?',
-        name: 'byIngredients',
-        component: MealsByIngredients,
-    },
+        component: DefaultLayout,
+        children: [
+            {
+                path: '/',
+                name: 'home',
+                component: Home,
+            },
+            {
+                path: '/by-name/:name?',
+                name: 'byName',
+                component: MealsByName,
+            },
+            {
+                path: '/by-letter/:letter?',
+                name: 'byLetter',
+                component: MealsByLetter,
+            },
+            {
+                path: '/by-ingredients/:ingredient?',
+                name: 'byIngredients',
+                component: MealsByIngredients,
+            },
+        ],
+
+    }
 ];
 
 const router = createRouter({
