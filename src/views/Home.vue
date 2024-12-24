11dae4c72040e8c71987ad9e56a5df165188bc3a
Andrew Chen <ssword@Andrews-Mac-mini.local>
2024-12-24 23:03:26 +0800

    initialized project and added store

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <pre>{{ meals }} </pre>
+  Home
+</template>
+
+<script setup>
+import { computed } from 'vue';
+// import store from '../store';
+import { useStore } from 'vuex';
+
+const store = useStore();
+const meals = computed(() => store.state.meals);
+console.log(meals);
+
+
+</script>
