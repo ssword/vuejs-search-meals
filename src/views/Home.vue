70b5577c8a35799aa1bd4916d6a33a23522267a8
Andrew Chen <ssword@Andrews-Mac-mini.local>
2024-12-25 00:00:39 +0800

    create home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,16 @@
 <template>
-  <pre>{{ meals }} </pre>
-  Home
+  <div class="flex flex-col p-8"> 
+
+  <input type="text" class="rounded border-2 border-gray-200 w-full" 
+  placeholder="Search for Meals" />
+
+  <div class="flex justify-center gap-2 mt-2">
+    <router-link :to="{name: 'byLetter', params: {letter}}" v-for="letter of letters" :key="letter">
+      {{ letter }}
+    </router-link>
+
+  </div>
+</div>
 </template>
 
 <script setup>
@@ -10,7 +20,7 @@ import { useStore } from 'vuex';
 
 const store = useStore();
 const meals = computed(() => store.state.meals);
-console.log(meals);
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 
 
 </script>
